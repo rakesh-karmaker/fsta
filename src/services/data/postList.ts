2eec91a70e2e58787b1453f3e7edae8d1b0a4cf5
rakesh-karmaker <rakeshkarmaker0175@gmail.com>
2025-04-23 22:49:36 +0600

    add more posts

--- src/services/data/postList.ts
+++ src/services/data/postList.ts
@@ -7,11 +7,46 @@ export type PostListType = {
 
 export const postList: PostListType[] = [
   {
+    title: "Article",
+    description:
+      "We're thrilled to welcome Starlink to Bangladesh! The Frontier Science and Tech Alliance (FSTA) is excited to see the incredible impact Starlink’s satellite internet will have on bridging the digital divide, bringing high-speed internet access to every corner of our country! 🇧🇩",
+    link: "https://www.facebook.com/fstaXoc/posts/pfbid02Jtib1E1hg8PjRbdVj5xEriv33gYvXxMHHm14yNU52AwPHqksGKw479BavQqTNzvdl",
+    image: "spacex.png",
+  },
+  {
+    title: "Article",
+    description:
+      "Markets are pricing in a cocktail of fear—rate cuts delayed, inflation sticky, geopolitical tensions rising, and earnings whispers not helping. Volatility is surging, liquidity’s drying up, and risk appetite is evaporating faster than hope on a red Wednesday. This isn’t just a correction—it’s a sentiment collapse.",
+    link: "https://www.facebook.com/fstaXoc/posts/pfbid065KqitRyh9Djvtcg3wYYQ6GcB7SrHS9buwdG4BGnH1ZNFk5tHdxh5mEXo4amjuQol",
+    image: "stock.png",
+  },
+  {
+    title: "Article",
+    description:
+      "Recently, Boeing and Airbus have effectively split the market for large commercial jets, with the two aerospace giants holding the lion's share of global aircraft production. Over time, China's COMAC (Commercial Aircraft Corporation of China) is expected to offer some competition, but for the foreseeable future, Boeing is poised to deliver thousands of aircraft to China, given the country's expanding aviation market and its demand for airliners.",
+    link: "https://www.facebook.com/fstaXoc/posts/pfbid032E6eLBxKF5P7hm1ib92Zx7LRAF5hoyW6PbCdZu6W7svqMvac6wA6vtyNEZmo3cyel",
+    image: "boing.png",
+  },
+  {
+    title: "Article",
+    description:
+      "An January, around 120 Starlink satellites from SpaceX reentered Earth’s atmosphere and burned up. With three to four reentries happening each day, these events created artificial meteor showers that were seen from various parts of the world.",
+    link: "https://www.facebook.com/fstaXoc/posts/pfbid0N7U751WUcn9uTmsskcgz3FVXTjkC7qkERZkbhJVNdNwgzNSKcPPJwq7gNQrjFMWdl",
+    image: "starlink.png",
+  },
+  {
     title: "Workshop",
     description:
       "This session is designed to guide you from basic to advanced levels, covering essential concepts in development, certifications, and problem-solving.",
     link: "https://www.facebook.com/fstaXoc/posts/pfbid02AY39TQKqTDaUJZNg2sBh28rr48e7iGRLSDcmAwZJvxpjFch9F3dDs814XdhMH2Khl",
     image: "post-1.png",
+  },
+  {
+    title: "Article",
+    description:
+      "The government of Bangladesh has officially renamed the Bangabandhu Satellite, marking a new milestone in the country’s space and communication sector. The announcement was made during a special event attended by government officials, representatives from the Bangladesh Telecommunication Regulatory Commission (BTRC), and key stakeholders from the technology and space industries.",
+    link: "https://www.facebook.com/fstaXoc/posts/pfbid088oSWc7RLBkdBCfkHesBMYGzWKHe2AqE16VhY9HLzvBxgzKY2G1D6Nx7PFbxVnp7l",
+    image: "bd-1.png",
   },
   {
     title: "Fusion",
